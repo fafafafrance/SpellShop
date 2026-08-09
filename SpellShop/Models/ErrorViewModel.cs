2a35af8299d48ba228bedc98c8a4241b14207e93
France A <133005536+fafafafrance@users.noreply.github.com>
2026-08-08 22:51:19 -0700

    Add project files.

---
+++ SpellShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SpellShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
